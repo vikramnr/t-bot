fc54a82ddfb1b3a14a94bffe95ded74d95b65ab1
vikramnr <vikram1912@protonmail.com>
2020-08-22 13:13:24 +0530

    initial app

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.post('/new-message', async (req, res) => {
+	const { message } = req.body
+
+	if (!message || message.text.toLowerCase().indexOf('hello') < 0) {
+		return res.end()
+	}
+    try {
+        let response = await axios.post(
+            `https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
+            {
+                chat_id: message.chat.id,
+                text: 'Hey there',
+            }
+        )
+        console.log(response)
+        res.end('ok')   
+    } catch (err) {
+        console.log(err)
+        res.end(err)
+    }
+	
+})
+
+app.listen(3000,() => {
+    console.log('server started at port 3000')
+})
