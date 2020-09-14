b44eb69946650e4df32e45fe3d097c32cf53a3aa
madhu <madhumjaya25@gmail.com>
2020-09-14 21:11:38 +0530

    restrict msg length

--- routes/index.js
+++ routes/index.js
@@ -18,15 +18,14 @@ router.post('/new-message', async (req, res) => {
 
 	if (!message || message.text.toLowerCase().indexOf('hello') < 0) {
         const date = moment().format("MMMM_DD")
-        console.log(date)
-        console.log(`https://en.wikipedia.org/w/rest.php/v1/page/${date}`)
         const response = await axios.get(`https://en.wikipedia.org/w/rest.php/v1/page/${date}`)
         let data = response.data.source.split('Events==')
-        data =  data[1].split('*')
+        data =  data[1].split('*').slice(0,10)
+        console.log(data)
 		try {
 			let response = await axios.post(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`, {
 				chat_id: message.chat.id,
-				text: data.slice(0,100),
+				text: data,
 			})
 			console.log(response)
 			res.end('ok')
