ced9d0a85687ea3e46e63e4ac76467bfa2c8248d
madhu <madhumjaya25@gmail.com>
2020-09-14 21:17:19 +0530

    added santizer for HTML

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const axios = require('axios')
 const moment = require('moment')
+const sanitizeHtml = require('sanitize-html');
+
 
 router.get('/',async(req,res) => {
     const date = moment().format("MMMM_DD")
@@ -21,6 +23,7 @@ router.post('/new-message', async (req, res) => {
         const response = await axios.get(`https://en.wikipedia.org/w/rest.php/v1/page/${date}`)
         let data = response.data.source.split('Events==')
         data =  data[1].split('*').slice(0,10)
+        data = sanitizeHtml(data)
         console.log(data)
 		try {
 			let response = await axios.post(`https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`, {
