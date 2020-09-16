34ae3fd227b48035ce4feb7deb59dbefeae45ba5
vikram <vikram1912@protonamil.com>
2020-09-16 17:30:50 +0530

    standard release

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.get("/", checkCmd , async (req, res) => {
 
 router.post("/new-message", checkCmd ,async (req, res) => {
   const { message } = req.body
+  console.log(req.body)
   const data =  req.body.cmdData
     try {
       let response = await axios.post(
