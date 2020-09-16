3a9b4676cedb46bf9f58a99ff7c70592e8c5d8d0
vikram <vikram1912@protonamil.com>
2020-09-16 18:09:01 +0530

    standard release

--- routes/index.js
+++ routes/index.js
@@ -1,33 +1,40 @@
 const express = require("express");
 const axios = require("axios");
-const cheerio = require('cheerio');
+const cheerio = require("cheerio");
 const router = express.Router();
-const { checkCmd } = require("./middleware");
+const { getWikiData, getOnThisDay } = require("./middleware");
 
-router.get("/", checkCmd , async (req, res) => {
-  let data = req.body.cmdData
-  console.log(data)
-  res.send('hello world')
+router.get("/", checkCmd, async (req, res) => {
+  let data = req.body.cmdData;
+  console.log(data);
+  res.send("hello world");
 });
 
-router.post("/new-message", checkCmd ,async (req, res) => {
-  const { message } = req.body
-  console.log(req.body)
-  const data =  req.body.cmdData
-    try {
-      let response = await axios.post(
-        `https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
-        {
-          chat_id: message.chat.id,
-          text: data
-        }
-      );
-      console.log(response);
-      res.end("ok");
-    } catch (err) {
-      console.log(err);
-      res.end(err);
+router.post("/new-message", async (req, res) => {
+  const { message } = req.body;
+  const cmdData = 'Hey there!!. This is a bot that sends the historical events for today. For more details use "help"';
+  if (message && message.text) {
+    const cmd = message.text.toLowerCase();
+    if (cmd === "wiki") {
+      cmdData = await getWikiData();
+    } else if (cmd === "onthisday") {
+      cmdData = await getOnThisDay();
     }
+    cmdData =
+      'Please use "wiki" for data from Wikipedia and "onthisday" for data from other websites';
+  }
+  try {
+    let response = await axios.post(
+      `https://api.telegram.org/bot${process.env.API_KEY}/sendMessage`,
+      {
+        chat_id: message.chat.id,
+        text: cmdData,
+      }
+    );
+    res.end("ok");
+  } catch (err) {
+    res.end(err);
+  }
 });
 
 module.exports = router;
